b5fcde4d9e9c23bb2d7bb3494a054abb9e836d40
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2024-05-03 14:35:34 +0100

    changed the loader to purple

--- src/Weather.js
+++ src/Weather.js
@@ -16,11 +16,12 @@ export default function Weather(props) {
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+
     return <Audio
     height="80"
     width="80"
     radius="9"
-    color="green"
+    color="purple"
     ariaLabel="loading"
     wrapperStyle
     wrapperClass
