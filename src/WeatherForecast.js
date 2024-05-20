2e1abd5c246d5cb051d17817245b461f8f15d338
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2024-05-20 13:45:54 +0100

    Added 5 day forecast and styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,54 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, {useState, useEffect} from "react";
 import "./WeatherForecast.css";
-import Axios from "axios";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 
 export default function WeatherForecast(props) {
-    function handleResponse(response) {
-        console.log(response.data);
-    }
-    let apiKey = "4f0252e46f5ob21t364250ae01f31bc7";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
 
-    Axios.get(apiUrl).then(handleResponse);
-    return (
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
+
+    function handleResponse(response) {
+        setForecast(response.data.daily);
+        setLoaded(true);
+        
+    }
+
+    function load() {
+        let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    if (loaded) {
+        return (
         <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">Fri</div>
-                    <WeatherIcon code = "13d" size={36} />
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">19
-                        </span>
-                        <span className="WeatherForecast-temperature-min">10
-                        </span>
-                    </div>
-                </div>
-                
-            </div>
+        <div className="row">
+            {forecast.map(function(dailyForecast, index) {
+                if (index < 5) {
+                return (
+                    <div className="col">
+                    <WeatherForecastDay data={dailyForecast} />
+                  </div>
+                );
+            } else {
+                return null;
+            }
+            })}
+           </div>
         </div>
-    )
+        ); 
+    } else {
+    load();
+   
+   return null;
+    
+    }
 }
